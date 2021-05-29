8232c0a0230852031f108f0366422aa87e45b66b
Denzmc <degtyar87@mail.ru>
2021-05-29 11:26:21 +0600

    String parser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,34 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val list: MutableList<String> = mutableListOf()
+        val map = mapOf('<' to '>', '(' to ')', '[' to ']')
+
+        var sec = ""
+
+        for (i in 0..inputString.length - 1){
+
+            sec = ""
+
+            if (map.containsKey(inputString[i])) {
+                val openKey = inputString[i]
+                val closeKey = map[inputString[i]]
+                var x = 1
+                for (j in i + 1 .. inputString.length - 1){
+                    if (openKey == inputString[j]){
+                        x++
+                    }
+                    if (inputString[j] == closeKey){
+                        x--
+                    }
+                    if (x == 0){
+                        list.add(sec)
+                        break
+                    }
+                    sec += inputString[j]
+                }
+            }
+        }
+        return list.toTypedArray()
     }
 }
